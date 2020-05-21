8f86e9eb5fff522ff83152340e70fcb174b25c17
deckard-austin <deckardaustin852@gmail.com>
2020-05-21 03:06:52 -0600

    redo commit

--- components/header.js
+++ components/header.js
@@ -1,30 +1,42 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Header = () => (
-  <header>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/post/[id]" as="/post/first">
-          <a>First Post</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/post/[id]" as="/post/second">
-          <a>Second Post</a>
-        </Link>
-      </li>
-    </ul>
-  </header>
-)
+  <p>Startupbootcamp - SE2020 - Mentor Matching</p>
+  // <header>
+  //   <ul>
+  //     <li>
+  //       <Link href='/'>
+  //         <a>Home</a>
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link href='/about'>
+  //         <a>About</a>
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link href='/post/[id]' as='/post/first'>
+  //         <a>First Post</a>
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link href='/post/[id]' as='/post/second'>
+  //         <a>Second Post</a>
+  //       </Link>
+  //     </li>
+  //     {/* This eventId is where we first pass down to an event */}
+  //     <li>
+  //       <Link href='/event/[eventId]' as={`/event/specificEvent1`}>
+  //         <a>A Specific Event #1</a>
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link href='/event/[eventId]' as={`/event/specificEvent2`}>
+  //         <a>A Specific Event #2</a>
+  //       </Link>
+  //     </li>
+  //   </ul>
+  // </header>
+);
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Header from '../components/header'
+import Header from '../components/Header';
+import StartupList from '../components/StartupList';
 
 const Home = () => (
-  <>
+  // Import startups, map through below
+  <div>
     <Header />
-    <h1>Hello World!</h1>
-  </>
-)
+    <h1>List of Startups</h1>
+    <h1>List of Mentors</h1>
+  </div>
+);
 
-export default Home
+export default Home;
